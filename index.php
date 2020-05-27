33d2cb78ff249ead0a7db8ac3ac48a3e70acb9b2
SorLAGG <kosdnaoskd@mail.ru>
2020-05-27 15:47:45 +0600

    123

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+$mark = array("iPhone", "Samsung Galaxy", "Nokia", "Samsung");
+for ($tr=1; $tr<=$mark.Length; $tr++){
+        echo '<tr>';
+        for ($td=1; $td<=3; $td++){
+            echo '<td>'. $tr*$td .'</td>';
+        }
+    echo '</tr>';
+}
+
+echo '</table>';
+
+?>
